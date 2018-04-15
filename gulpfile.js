7fab51380d6de09c438c3a0f71fa6e82cf616ce3
Astaton <orson108@hotmail.com>
2018-04-15 19:33:41 -0400

    configured gulp and webpack files

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const gulpAutoprefix = require('gulp-autoprefixer');
+const gulpHtmlMini = require('gulp-htmlmin');
+
+const src_html = 'public/*.html';
+const src_css = 'src/*.css';
+const dest_html = 'public/';
+const dest_css = 'src/';
+
+gulp.task('prefixes', function (){
+	gulp.src(src_css)
+		.pipe(gulpAutoprefix({
+			browsers:['cover 99.5% in US']
+		}))
+		.pipe(gulp.dest(dest_css))
+});
+
+gulp.task('htmlMini', function(){
+	gulp.src(src(src_html))
+		.pipe(gulpHtmlMini({
+			collapseWhitespace: true,
+			minifyCSS: true
+		}))
+});
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+const htmlWebpack = require('html-webpack-plugin');
+
+module.exports = {
+	entry: {
+		script: './src/script.js'
+	},
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: '[name].bundle.js'
+	},
+	module: {
+		rules: [{
+			test: /\.js$/.
+			exclude: /node_modules/,
+			loader: 'bable-loader',
+			query: {
+				presets: ['env']
+			}
+		},
+		{
+			test: /\.css$/,
+			use: [
+			'style-loader',
+			'css-loader'
+			]
+		}]
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery'
+		}),
+		new htmlWebpack({
+			template: path.join(__dirname, 'src', 'index.html'),
+			inject: 'body'
+		})
+	]
+};
